aeae0ba5677284657807c3eabe0a3ea09a40ab1f
George-Weng <george_yh_weng@outlook.com>
2026-08-26 12:38:04 +0800

    Refactor trade calculation into separate function

--- functions/api/trades/batch.js
+++ functions/api/trades/batch.js
@@ -1,40 +1,4 @@
-import { verifyJWT } from '../../_utils';
-
-// 同样需要计算函数（或从外部导入，这里为了完整复制一份）
-async function calculateTrade(db, tradeData) {
-    const symbolConfig = await db.prepare('SELECT * FROM symbols WHERE symbol = ?').bind(tradeData.symbol).first();
-    if (!symbolConfig) {
-        return { ...tradeData, profit: 0, profit_points: 0, open_fee: 0, close_fee: 0, point_value: 0, tick_size: 0 };
-    }
-    const { point_value, tick_size, open_fee_rate, close_fee_rate } = symbolConfig;
-    const openPrice = tradeData.open_price;
-    const closePrice = tradeData.close_price;
-    const volume = tradeData.volume;
-    const direction = tradeData.direction;
-
-    const openFee = openPrice * volume * open_fee_rate;
-    const closeFee = closePrice ? closePrice * volume * close_fee_rate : 0;
-
-    let profitPoints = 0;
-    if (closePrice !== null && closePrice !== undefined) {
-        if (direction === '买入') {
-            profitPoints = (closePrice - openPrice) / tick_size * volume;
-        } else if (direction === '卖出') {
-            profitPoints = (openPrice - closePrice) / tick_size * volume;
-        }
-    }
-    const profit = profitPoints * point_value * volume - openFee - closeFee;
-
-    return {
-        ...tradeData,
-        profit: parseFloat(profit.toFixed(2)),
-        profit_points: parseFloat(profitPoints.toFixed(2)),
-        open_fee: parseFloat(openFee.toFixed(2)),
-        close_fee: parseFloat(closeFee.toFixed(2)),
-        point_value,
-        tick_size,
-    };
-}
+import { verifyJWT, calculateTrade } from '../../_utils';
 
 export async function onRequest(context) {
     const { request, env } = context;
@@ -63,29 +27,21 @@ export async function onRequest(context) {
         else if (role === 'admin' && !targetUser) continue;
         else if (!targetUser) targetUser = username;
 
-        // 计算盈亏
         const calculated = await calculateTrade(db, { ...item, username: targetUser });
 
         const { success } = await db.prepare(
             `INSERT INTO trades 
             (username, date, symbol, direction, stop_loss, open_price, volume, close_price, 
-             profit, profit_points, open_fee, close_fee, point_value, tick_size)
-            VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`
+             profit, profit_points, open_fee, close_fee, point_value, tick_size, tp1, tp2)
+            VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`
         ).bind(
-            calculated.username,
-            calculated.date,
-            calculated.symbol,
-            calculated.direction,
-            calculated.stop_loss || null,
-            calculated.open_price,
-            calculated.volume,
+            calculated.username, calculated.date, calculated.symbol, calculated.direction,
+            calculated.stop_loss || null, calculated.open_price, calculated.volume,
             calculated.close_price || null,
-            calculated.profit,
-            calculated.profit_points,
-            calculated.open_fee,
-            calculated.close_fee,
-            calculated.point_value,
-            calculated.tick_size
+            calculated.profit, calculated.profit_points,
+            calculated.open_fee, calculated.close_fee,
+            calculated.point_value, calculated.tick_size,
+            calculated.tp1, calculated.tp2
         ).run();
         if (success) inserted++;
     }
